f91fada71dddfd9178dfe24863ff9badbf86382d
jay <90603347+jaychou123456@users.noreply.github.com>
2023-07-07 13:15:45 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,23 @@
+const app = require('express')();
+const server = require('http').createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', socket => {
+    console.log('New client connected!');
+
+    socket.on('chat message', msg => {
+        console.log(msg);
+        io.emit('chat message', msg);
+    }); 
+
+    socket.on('close', () => {
+        console.log('Client disconnected!');
+    });
+});
+
+server.listen(3000, () => 'listening on *:3000');
